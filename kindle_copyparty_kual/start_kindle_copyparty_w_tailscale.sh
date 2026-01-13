64402a6b5cd2f1a7d58d90718f2f37366f505e0a
Sanjiv Lobo <13509204+Xndr7@users.noreply.github.com>
2026-01-13 07:12:41 +0530

    Create start_kindle_copyparty_w_tailscale.sh

---
+++ kindle_copyparty_kual/start_kindle_copyparty_w_tailscale.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ -f /mnt/us/extensions/kindle_copyparty/kindle_copyparty.sh ] && [ -f /mnt/us/extensions/kindle_copyparty/start_kindle_copyparty.sh ] && [ -f /mnt/us/extensions/kindle_copyparty/run_copyparty_with_tailscale.sh ] && [ -f /etc/upstart/kindle_copyparty.conf ] && [ -f /mnt/us/extensions/kindle_copyparty/kindle_copyparty.ext3 ] ; then
+    /mnt/us/extensions/kterm/bin/kterm -e "sh /mnt/us/extensions/kindle_copyparty/run_copyparty_with_tailscale.sh" -k 1 -o U -s 7
+else
+	fbink -pmh -y -5 "Error: Required files missing. Deploy Alpine first!"
+fi
