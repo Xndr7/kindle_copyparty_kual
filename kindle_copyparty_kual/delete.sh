cccea8bbc4d297d5228dd839ab8959c31f6b37db
Sanjiv Lobo <13509204+Xndr7@users.noreply.github.com>
2026-01-13 11:21:54 +0530

    Update delete.sh

--- kindle_copyparty_kual/delete.sh
+++ kindle_copyparty_kual/delete.sh
@@ -3,7 +3,7 @@
 BASE_FOLDER="/mnt/us/extensions/kindle_copyparty"
 
 while true; do
-	read -p "Are you sure you want to delete Alpine Linux? This will remove the files /mnt/us/alpine.{ext3,sh,log,conf,zip} and /etc/upstart/alpine.conf (type y or n): " yn
+	read -p "Are you sure you want to delete Kindle Copyparty? This will remove the files {ext3,sh,log,conf,zip}(type y or n): This will NOT remove the Copyparty related files and directories as well as the Tailscale related files and directories(if any) " yn
 	case $yn in
 		[Yy]* ) echo "Deleting..."; break;;
 		[Nn]* ) echo "Aborted."; sh press_any_key.sh; exit;;
@@ -13,7 +13,7 @@ done
 
 rm $BASE_FOLDER/kindle_copyparty.ext3
 rm $BASE_FOLDER/kindle_copyparty.sh
-rm $BASE_FOLDER/main.log
+rm $BASE_FOLDER/start_kindle_copyparty.sh
 rm $BASE_FOLDER/kindle_copyparty.conf
 rm $BASE_FOLDER/kindle_copyparty.zip
 while [ -f /etc/upstart/kindle_copyparty.conf ] ; do
